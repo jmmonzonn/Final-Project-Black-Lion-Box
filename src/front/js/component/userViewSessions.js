8061d6a89dd15409d4d049e21675575b8115c24d
jorgepardor <jorgeluis.mail@gmail.com>
2022-03-24 18:12:39 +0000

    Agregados componentes para el dashboard del usuario

---
+++ src/front/js/component/userViewSessions.js
@@ -0,0 +1,40 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+export const ViewSessions = () => {
+  return (
+    <div className="container">
+      <p>Próximas sesiones disponibles:</p>
+      <table class="table table-striped">
+        <thead>
+          <tr>
+            <th scope="col">Día</th>
+            <th scope="col">Hora de inicio</th>
+            <th scope="col">Duración</th>
+            <th scope="col">Nombre de la clase</th>
+            <th scope="col">Nombre del profesor</th>
+            <th scope="col">Acciones</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>01/01/2020</td>
+            <td>15:00</td>
+            <td>120 minutos</td>
+            <td>Kung Fu 101</td>
+            <td>Tesako Tumoko</td>
+            <td>
+              <button>📝</button>
+              <button>❌</button>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+      <div className="ml-auto">
+        <Link to="/demo">
+          <button className="btn btn-primary">Apuntarse a una sesión</button>
+        </Link>
+      </div>
+    </div>
+  );
+};
