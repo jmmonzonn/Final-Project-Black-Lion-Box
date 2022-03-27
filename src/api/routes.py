b4d91b44bddcdd05d6141315264776adf37cd880
jorgepardor <jorgeluis.mail@gmail.com>
2022-03-27 10:40:48 +0000

    Corregido routes.py, ahora muestra bien los errores al crear un user. Editado el gitignore para excluir la carpeta migrations

--- src/api/routes.py
+++ src/api/routes.py
@@ -4,6 +4,8 @@ This module takes care of starting the API Server, Loading the DB and Adding the
 from flask import Flask, request, jsonify, url_for, Blueprint
 from api.models import db, User, Role, Suscription, Sessions, Sessions_type
 from api.utils import generate_sitemap, APIException
+from sqlalchemy.exc import IntegrityError
+from psycopg2.errors import NotNullViolation, UniqueViolation
 
 api = Blueprint('api', __name__)
 
@@ -21,58 +23,24 @@ def create_user():
     conditions_terms = request.json.get("conditions_terms", None)
     marketing_comunication = request.json.get("marketing_comunication", None)
     info = request.json.get("info", None)
-    is_active = request.json.get("is_active", None)
-  
-    not_unique_username = User.query.filter_by(username = username).first()
-    if not_unique_username != None:
-        return jsonify({"message": "Este usuario ya existe, prueba con otro."})  
 
-    not_unique_email = User.query.filter_by(email=email).first()
-    if not_unique_email != None:
-        return jsonify({"message": "Este email ya existe, prueba con otro."})
+    # if None in [username, password, email, phone, first_name, last_name, conditions_terms]:
+    #     return jsonify({"message" : "Alguno de los campos obligatorios no furula"}),400
+    try:
+        new_user = User(username = username, password = password, email = email, phone = phone, 
+                        first_name = first_name, last_name = last_name, adress = adress, 
+                        avatar_url = avatar_url, conditions_terms = conditions_terms, 
+                        marketing_comunication = marketing_comunication, info = info, is_active = False)
+        db.session.add(new_user)
+        db.session.commit()
+    except IntegrityError as ex:
+        print(ex)
+        if isinstance(ex.orig, NotNullViolation):
+            return jsonify({"message" : "Los campos son obligatorios no pueden quedar como null."}),400
+        elif isinstance(ex.orig, UniqueViolation):
+            return jsonify({"message" : "Ya se encuentra un usuario regisgtrado con estos datos"}),400
+        return jsonify({"message" : ex.orig}),400 
 
-    not_unique_phone = User.query.filter_by(phone=phone).first()
-    if not_unique_phone != None:
-        return jsonify({"message": "Este teléfono ya existe, prueba con otro."})
-
-    if username == '' or username == None:
-        return jsonify({"message": "Introduce un usuario."}), 401
-        
-    if password == '' or password == None:
-        return jsonify({"message": "Introduce una contraseña."}), 401
-        
-    if email == '' or email == None:
-        return jsonify({"message": "Introduce un email."}), 401
-
-    if phone == '' or phone == None:
-        return jsonify({"message": "Introduce un número de teléfono."}), 401
-
-    if first_name == '' or first_name == None:
-        return jsonify({"message": "Introduce un nombre."}), 401
-
-    if last_name == '' or last_name == None:
-        return jsonify({"message": "Introduce un apellido."}), 401
-
-    if avatar_url == '' or avatar_url == None:
-            return jsonify({"message": "Introduce un avatar."}), 401
-
-    if conditions_terms == False or conditions_terms == None:
-        return jsonify({"message": "Acepta los términos y condiciones."}), 401
-
-    if marketing_comunication == False or marketing_comunication == None:
-        return jsonify({"message": "Acepta los el marketing, puto ruso."}), 401
-
-    if info == '' or info == None:
-        return jsonify({"message": "Introduce la info, verga, marico."}), 401
-
-    if is_active == None:
-        return jsonify({"message": "Estás active?"}), 401
-
-      # Query your database for username and password
-    new_user = User(username = username, password = password, email = email, phone = phone, first_name = first_name, last_name = last_name, adress = adress, avatar_url = avatar_url, conditions_terms = conditions_terms, marketing_comunication = marketing_comunication, info = info, is_active = is_active)
-    db.session.add(new_user)
-    db.session.commit()
-    
     return jsonify({"message" : "Usuario registrado"}),200
 
 
