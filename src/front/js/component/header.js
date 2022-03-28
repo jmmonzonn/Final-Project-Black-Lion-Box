29db72e0d3e0c91803309b971e0034c310e2787f
jorgepardor <jorgeluis.mail@gmail.com>
2022-03-28 17:14:20 +0000

    Ahora si sirve el video

--- src/front/js/component/header.js
+++ src/front/js/component/header.js
@@ -1,13 +1,15 @@
 import React from "react";
-import video from "./../../video/headervideob.mp4";
 
 export const MainHeader = () => {
   return (
     <div className="container">
       <header>
         <div>Welcome to my site!</div>
-        <video autoPlay loop muted>
-          <source src={video} />
+        <video className="w-100 h-100" autoPlay muted loop>
+          <source
+            src="https://res.cloudinary.com/jorgepardor/video/upload/v1648485644/pexels-tima-miroshnichenko-6388436_2_x8rkwx.mp4"
+            type="video/mp4"
+          />
           Your browser does not support the video tag.
         </video>
       </header>
