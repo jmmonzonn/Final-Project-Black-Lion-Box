2e021bf2cd19604ec3141cd09618d2584830e3ad
CHUSGF <chusgf2906@gmail.com>
2022-04-02 12:19:59 +0000

    cambiando estilos al componente features

--- src/front/js/component/features.js
+++ src/front/js/component/features.js
@@ -3,69 +3,96 @@ import React from "react";
 export const Features = () => {
   return (
     <div className="container">
-      <div className="row">
-        <div className="col">
-          <h5 className="card-title">Card title</h5>
-          <p className="card-text">
-            Some quick example text to build on the card title and make up the
-            bulk of the card's content.
-          </p>
-          <a href="#" className="btn btn-primary">
-            Go somewhere
-          </a>
-          <div className="col">
-            <h5 class="card-title">Card title</h5>
-            <p className="card-text">
-              Some quick example text to build on the card title and make up the
-              bulk of the card's content.
-            </p>
-            <a href="#" className="btn btn-primary">
-              Go somewhere
-            </a>
+      <div className="row ">
+        <div className="col mb-5">
+          <div className="card text-center" style={{ width: 25 + "rem" }}>
+            <div className="card-body">
+              <h5 className="card-title">Special title treatment</h5>
+              <p className="card-text">
+                With supporting text below as a natural lead-in to additional
+                content.
+              </p>
+
+              <a href="#" className="btn btn-danger">
+                Start
+              </a>
+            </div>
           </div>
         </div>
-        <div className="row">
-          <div className="col">
-            <h5 class="card-title">Card title</h5>
-            <p className="card-text">
-              Some quick example text to build on the card title and make up the
-              bulk of the card's content.
-            </p>
-            <a href="#" className="btn btn-primary">
-              Go somewhere
-            </a>
-          </div>
-          <div className="col">
-            <h5 class="card-title">Card title</h5>
-            <p className="card-text">
-              Some quick example text to build on the card title and make up the
-              bulk of the card's content.
-            </p>
-            <a href="#" className="btn btn-primary">
-              Go somewhere
-            </a>
+        <div className="col mb-5">
+          <div className="card text-center" style={{ width: 25 + "rem" }}>
+            <div className="card-body">
+              <h5 className="card-title">Special title treatment</h5>
+              <p className="card-text">
+                With supporting text below as a natural lead-in to additional
+                content.
+              </p>
+              <a href="#" className="btn btn-danger">
+                Start
+              </a>
+            </div>
           </div>
         </div>
-        <div className="row">
-          <div className="col">
-            <h5 class="card-title">Card title</h5>
-            <p className="card-text">
-              Some quick example text to build on the card title and make up the
-              bulk of the card's content.
-            </p>
-            <a href="#" className="btn btn-primary">
-              Go somewhere
-            </a>
+      </div>
+
+      <div className="row ">
+        <div className="col mb-5">
+          <div className="card text-center" style={{ width: 25 + "rem" }}>
+            <div className="card-body">
+              <h5 className="card-title">Special title treatment</h5>
+              <p className="card-text">
+                With supporting text below as a natural lead-in to additional
+                content.
+              </p>
+              <a href="#" className="btn btn-danger">
+                Start
+              </a>
+            </div>
           </div>
-          <div className="col">
-            <h5 class="card-title">Card title</h5>
-            <p className="card-text">
-              Some quick example text to build on the card title and make up the
-              bulk of the card's content.
-            </p>
-            <a href="#" className="btn btn-primary">
-              Go somewhere
-            </a>
+        </div>
+        <div className="col mb-5">
+          <div className="card text-center" style={{ width: 25 + "rem" }}>
+            <div className="card-body">
+              <h5 className="card-title">Special title treatment</h5>
+              <p className="card-text">
+                With supporting text below as a natural lead-in to additional
+                content.
+              </p>
+              <a href="#" className="btn btn-danger">
+                Start
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="row ">
+        <div className="col mb-5">
+          <div className="card text-center" style={{ width: 25 + "rem" }}>
+            <div className="card-body">
+              <h5 className="card-title">Special title treatment</h5>
+              <p className="card-text">
+                With supporting text below as a natural lead-in to additional
+                content.
+              </p>
+              <a href="#" className="btn btn-danger">
+                Start
+              </a>
+            </div>
+          </div>
+        </div>
+        <div className="col mb-5">
+          <div className="card text-center" style={{ width: 25 + "rem" }}>
+            <div className="card-body">
+              <h5 className="card-title">Special title treatment</h5>
+              <p className="card-text">
+                With supporting text below as a natural lead-in to additional
+                content.
+              </p>
+              <a href="#" className="btn btn-danger">
+                Start
+              </a>
+            </div>
           </div>
         </div>
       </div>
