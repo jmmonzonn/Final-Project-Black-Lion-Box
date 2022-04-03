847af7934bf26d20448f1769179c363bb4ee19e5
CHUSGF <chusgf2906@gmail.com>
2022-04-03 09:46:29 +0000

    Actualizando componente Features

--- src/front/js/component/features.js
+++ src/front/js/component/features.js
@@ -3,95 +3,25 @@ import React from "react";
 export const Features = () => {
   return (
     <div className="container">
-      <div className="row ">
-        <div className="col mb-5">
-          <div className="card text-center" style={{ width: 25 + "rem" }}>
-            <div className="card-body">
-              <h5 className="card-title">Special title treatment</h5>
-              <p className="card-text">
-                With supporting text below as a natural lead-in to additional
-                content.
-              </p>
-
-              <a href="#" className="btn btn-danger">
-                Start
-              </a>
-            </div>
-          </div>
-        </div>
-        <div className="col mb-5">
-          <div className="card text-center" style={{ width: 25 + "rem" }}>
-            <div className="card-body">
-              <h5 className="card-title">Special title treatment</h5>
-              <p className="card-text">
-                With supporting text below as a natural lead-in to additional
-                content.
-              </p>
-              <a href="#" className="btn btn-danger">
-                Start
-              </a>
-            </div>
-          </div>
-        </div>
-      </div>
-
-      <div className="row ">
-        <div className="col mb-5">
-          <div className="card text-center" style={{ width: 25 + "rem" }}>
-            <div className="card-body">
-              <h5 className="card-title">Special title treatment</h5>
-              <p className="card-text">
-                With supporting text below as a natural lead-in to additional
-                content.
-              </p>
-              <a href="#" className="btn btn-danger">
-                Start
-              </a>
-            </div>
-          </div>
-        </div>
-        <div className="col mb-5">
-          <div className="card text-center" style={{ width: 25 + "rem" }}>
-            <div className="card-body">
-              <h5 className="card-title">Special title treatment</h5>
-              <p className="card-text">
-                With supporting text below as a natural lead-in to additional
-                content.
-              </p>
-              <a href="#" className="btn btn-danger">
-                Start
-              </a>
-            </div>
-          </div>
-        </div>
-      </div>
-
-      <div className="row ">
-        <div className="col mb-5">
-          <div className="card text-center" style={{ width: 25 + "rem" }}>
-            <div className="card-body">
-              <h5 className="card-title">Special title treatment</h5>
-              <p className="card-text">
-                With supporting text below as a natural lead-in to additional
-                content.
-              </p>
-              <a href="#" className="btn btn-danger">
-                Start
-              </a>
-            </div>
-          </div>
-        </div>
-        <div className="col mb-5">
-          <div className="card text-center" style={{ width: 25 + "rem" }}>
-            <div className="card-body">
-              <h5 className="card-title">Special title treatment</h5>
-              <p className="card-text">
-                With supporting text below as a natural lead-in to additional
-                content.
-              </p>
-              <a href="#" className="btn btn-danger">
-                Start
-              </a>
+      <div className="row">
+        <div className="col">
+          <div className="d-flex flip-card">
+            <div className="flip-card-inner">
+              <div className="flip-card-front">
+                <img
+                  className=" image"
+                  src="https://res.cloudinary.com/blacklionbox/image/upload/v1648973804/geraete_flip_kachel_02_320x480_msvsji.webp"
+                  alt="Cardio"
+                />
+              </div>
+              <div className="flip-card-back">
+                <p>
+                  Mejora tu resistencia, condición física y capacidad de
+                  regeneración utilizando cintas de correr, crosstrainers,
+                  steppers, máquinas de remo y mucho más.
+                </p>
+                <button className="fill-button button-block">Máquinas</button>
+              </div>
             </div>
           </div>
         </div>
