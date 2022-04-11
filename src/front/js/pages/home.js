87dc434c85a5e6a712b8711185ad98c548052924
jorgepardor <jorgeluis.mail@gmail.com>
2022-04-11 18:38:44 +0000

    Cambios en el home, actualizado el video y creado el componente de ver las suscripciones features

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -6,6 +6,7 @@ import { MainHeader } from "../component/header.js";
 import { Maps } from "../component/maps";
 import { Contact } from "../component/contact";
 import { Features } from "../component/features";
+import { SubscriptionTiers } from "../component/subscriptionTiers";
 
 export const Home = () => {
   const { store, actions } = useContext(Context);
@@ -13,6 +14,7 @@ export const Home = () => {
   return (
     <div className="text-center">
       <MainHeader />
+      <SubscriptionTiers />
       <Features />
       <Maps />
       <Contact />
