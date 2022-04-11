b7b81d19bd2a4a9389c45c951f4774418206c867
jorgepardor <jorgeluis.mail@gmail.com>
2022-04-11 19:43:48 +0000

    Cambiada la maquetación de register a TailwindCSS

--- src/front/js/component/register.js
+++ src/front/js/component/register.js
@@ -8,144 +8,253 @@ export const Register = () => {
 
   return (
     <div className="container">
-      <div className="row justify-content-center">
-        <div className="col-3 mx-auto">
+      <form>
+        <div class="relative z-0 mb-6 w-full group">
           <input
-            type="text"
+            type="user"
             onChange={(e) => {
               setUser({ ...user, username: e.target.value });
             }}
-            className="form-control py-1 my-2"
-            placeholder="Introduce usuario"
-          ></input>
+            name="floating_user"
+            class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+            placeholder=" "
+            required
+          />
+          <label
+            for="floating_user"
+            class="absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+          >
+            Nombre de Usuario:
+          </label>
+        </div>
 
-          <input
-            type="password"
-            onChange={(e) => {
-              setUser({ ...user, password: e.target.value });
-            }}
-            className="form-control py-1 my-2"
-            placeholder="Introduce contraseña"
-          ></input>
-
+        <div class="relative z-0 mb-6 w-full group">
           <input
             type="email"
             onChange={(e) => {
               setUser({ ...user, email: e.target.value });
             }}
-            className="form-control py-1 my-2"
-            placeholder="Introduce email"
-          ></input>
-
+            name="floating_email"
+            class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+            placeholder=" "
+            required
+          />
+          <label
+            for="floating_email"
+            class="absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+          >
+            Correo Electrónico
+          </label>
+        </div>
+        <div class="relative z-0 mb-6 w-full group">
           <input
-            type="text"
+            type="password"
             onChange={(e) => {
-              setUser({ ...user, phone: e.target.value });
+              setUser({ ...user, password: e.target.value });
             }}
-            className="form-control py-1 my-2"
-            placeholder="Introduce teléfono"
-          ></input>
-
+            name="floating_password"
+            id="floating_password"
+            class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+            placeholder=" "
+            required
+          />
+          <label
+            for="floating_password"
+            class="absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+          >
+            Contraseña
+          </label>
+        </div>
+        <div class="grid xl:grid-cols-2 xl:gap-6">
+          <div class="relative z-0 mb-6 w-full group">
+            <input
+              type="text"
+              name="floating_first_name"
+              onChange={(e) => {
+                setUser({ ...user, first_name: e.target.value });
+              }}
+              id="floating_first_name"
+              class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+              placeholder=" "
+              required
+            />
+            <label
+              for="floating_first_name"
+              class="absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+            >
+              Nombre
+            </label>
+          </div>
+          <div class="relative z-0 mb-6 w-full group">
+            <input
+              type="text"
+              onChange={(e) => {
+                setUser({ ...user, last_name: e.target.value });
+              }}
+              name="floating_last_name"
+              id="floating_last_name"
+              class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+              placeholder=" "
+              required
+            />
+            <label
+              for="floating_last_name"
+              class="absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+            >
+              Apellido
+            </label>
+          </div>
+        </div>
+        <div class="relative z-0 mb-6 w-full group">
           <input
-            type="text"
-            onChange={(e) => {
-              setUser({ ...user, first_name: e.target.value });
-            }}
-            className="form-control py-1 my-2"
-            placeholder="Introduce nombre"
-          ></input>
-
-          <input
-            type="text"
-            onChange={(e) => {
-              setUser({ ...user, last_name: e.target.value });
-            }}
-            className="form-control py-1 my-2"
-            placeholder="Introduce apellido"
-          ></input>
-
-          <input
-            type="text"
+            type="direction"
             onChange={(e) => {
               setUser({ ...user, adress: e.target.value });
             }}
-            className="form-control py-1 my-2"
-            placeholder="Introduce dirección"
-          ></input>
-
-          <input
-            type="text"
-            onChange={(e) => {
-              setUser({ ...user, avatar_url: e.target.value });
-            }}
-            className="form-control py-1 my-2"
-            placeholder="Introduce url de tu avatar"
-          ></input>
-
-          <input
-            type="text"
-            onChange={(e) => {
-              setUser({ ...user, info: e.target.value });
-            }}
-            className="form-control py-1 my-2"
-            placeholder="Introduce info"
-          ></input>
-
-          <div class="form-check">
-            <input
-              type="checkbox"
-              className="form-check-input"
-              id="exampleCheck1"
-              onClick={() => {
-                user.conditions_terms
-                  ? user.conditions_terms == true
-                    ? setUser({ conditions_terms: false })
-                    : setUser({ conditions_terms: true })
-                  : setUser({ ...user, conditions_terms: true });
-              }}
-            ></input>
-            <label className="form-check-label" for="exampleCheck1">
-              Aceptar términos y condiciones
-            </label>
-          </div>
-
-          <div class="form-check">
-            <input
-              type="checkbox"
-              className="form-check-input"
-              id="exampleCheck1"
-              onClick={() => {
-                user.marketing_comunication
-                  ? user.marketing_comunication == true
-                    ? setUser({ marketing_comunication: false })
-                    : setUser({ marketing_comunication: true })
-                  : setUser({ ...user, marketing_comunication: true });
-              }}
-            ></input>
-            <label className="form-check-label" for="exampleCheck1">
-              Marketing y comunicaciones
-            </label>
-          </div>
-
-          <button
-            type="button"
-            onClick={() => {
-              fetch(process.env.BACKEND_URL + "/api/signup", {
-                method: "POST",
-                headers: {
-                  "Content-Type": "application/json",
-                },
-                body: JSON.stringify(user),
-              })
-                .then((resp) => resp.json())
-                .then((data) => console.log(data));
-            }}
-            className="btn btn-danger mx-auto px-auto"
+            name="floating_direction"
+            class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+            placeholder=" "
+            required
+          />
+          <label
+            for="floating_direction"
+            class="absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
           >
-            Registro
-          </button>
+            Dirección
+          </label>
         </div>
-      </div>
+        <div class="grid xl:grid-cols-2 xl:gap-6">
+          <div class="relative z-0 mb-6 w-full group">
+            <input
+              type="tel"
+              onChange={(e) => {
+                setUser({ ...user, phone: e.target.value });
+              }}
+              pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}"
+              name="floating_phone"
+              id="floating_phone"
+              class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+              placeholder=" "
+              required
+            />
+            <label
+              for="floating_phone"
+              class="absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+            >
+              Teléfono
+            </label>
+          </div>
+          <div class="relative z-0 mb-6 w-full group">
+            <input
+              type="text"
+              onChange={(e) => {
+                setUser({ ...user, info: e.target.value });
+              }}
+              name="floating_info"
+              id="floating_info"
+              class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+              placeholder=" "
+              required
+            />
+            <label
+              for="floating_info"
+              class="absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+            >
+              Información adicional
+            </label>
+          </div>
+          <div class="grid xl:grid-cols-2 xl:gap-6">
+            <div class="relative z-0 mb-6 w-full group">
+              <div class="flex items-start mb-6">
+                <div class="flex items-center h-5">
+                  <input
+                    id="terms"
+                    aria-describedby="terms"
+                    type="checkbox"
+                    onClick={() => {
+                      user.conditions_terms
+                        ? user.conditions_terms == true
+                          ? setUser({ conditions_terms: false })
+                          : setUser({ conditions_terms: true })
+                        : setUser({ ...user, conditions_terms: true });
+                    }}
+                    class="w-4 h-4 bg-gray-50 rounded border border-gray-300 focus:ring-3 focus:ring-blue-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-blue-600 dark:ring-offset-gray-800"
+                    required
+                  />
+                </div>
+                <div class="ml-3 text-sm">
+                  <label
+                    for="terms"
+                    class="font-medium text-gray-900 dark:text-gray-300"
+                  >
+                    Acepto los{" "}
+                    <a
+                      href="#"
+                      class="text-blue-600 hover:underline dark:text-blue-500"
+                    >
+                      términos y condiciones.
+                    </a>
+                  </label>
+                </div>
+              </div>
+            </div>
+            <div class="relative z-0 mb-6 w-full group">
+              <div class="flex items-start mb-6">
+                <div class="flex items-center h-5">
+                  <input
+                    id="mkt"
+                    aria-describedby="mkt"
+                    type="checkbox"
+                    onClick={() => {
+                      user.marketing_comunication
+                        ? user.marketing_comunication == true
+                          ? setUser({ marketing_comunication: false })
+                          : setUser({ marketing_comunication: true })
+                        : setUser({ ...user, marketing_comunication: true });
+                    }}
+                    class="w-4 h-4 bg-gray-50 rounded border border-gray-300 focus:ring-3 focus:ring-blue-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-blue-600 dark:ring-offset-gray-800"
+                    required
+                  />
+                </div>
+                <div class="ml-3 text-sm">
+                  <label
+                    for="mkt"
+                    class="font-medium text-gray-900 dark:text-gray-300"
+                  >
+                    Acepto el envío de comunicaciones de marketing
+                  </label>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <button
+          type="submit"
+          onClick={() => {
+            fetch(process.env.BACKEND_URL + "/api/signup", {
+              method: "POST",
+              headers: {
+                "Content-Type": "application/json",
+              },
+              body: JSON.stringify(user),
+            })
+              .then((resp) => resp.json())
+              .then((data) => console.log(data));
+          }}
+          class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+        >
+          Registro
+        </button>
+      </form>
     </div>
+
+    //       <input
+    //         type="text"
+    // onChange={(e) => {
+    //   setUser({ ...user, avatar_url: e.target.value });
+    // }}
+    //         className="form-control py-1 my-2"
+    //         placeholder="Introduce url de tu avatar"
+    //       ></input>
   );
 };
