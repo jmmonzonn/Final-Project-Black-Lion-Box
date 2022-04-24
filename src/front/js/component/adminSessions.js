47b96f3c2c8d31f0dce6999493471d89fe3b1ebe
jmmonzonn <jmmonzonn@gmail.com>
2022-04-24 13:06:21 +0000

    adminSessions button type submit error solucionado, falta llevarlo a los demas componentes

--- src/front/js/component/adminSessions.js
+++ src/front/js/component/adminSessions.js
@@ -262,7 +262,8 @@ export const AdminSessions = () => {
                   </div>
                 </div>
                 <button
-                  type="submit"
+                  type="button"
+                  data-modal-toggle="session-modal"
                   className="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
                   onClick={() => {
                     fetch(process.env.BACKEND_URL + "/api/sessions", {
