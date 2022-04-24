2ff95283538a3e415b9fe30f81d7b1319a3fa526
jorgepardor <jorgeluis.mail@gmail.com>
2022-04-24 22:19:07 +0000

    Cambios en el home, el logo funciona... falta alinear un poco los botones

--- src/front/js/component/maps.js
+++ src/front/js/component/maps.js
@@ -12,6 +12,7 @@ export const Maps = () => {
         src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2893.742910383067!2d-5.680530284638588!3d43.50769847912654!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xd367d99c79b32d9%3A0xd121ce325c520c1!2sBlack%20Lion%20Box!5e0!3m2!1ses!2ses!4v1648589570010!5m2!1ses!2ses"
         width="100%"
         height="450"
+        className="dark:opacity-50 dark:invert"
         style={{ border: "0" }}
         allowFullScreen=""
         loading="lazy"
