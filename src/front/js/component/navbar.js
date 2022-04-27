27bb15a0ab83af866cb23698e1f9614fb4f09df2
jorgepardor <jorgeluis.mail@gmail.com>
2022-04-27 22:08:17 +0000

    No podía irme a dormir sin quitarle el padding al botón de logout

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -9,7 +9,7 @@ export const Navbar = () => {
     return (
       <button
         type="button"
-        className="py-2.5 px-5 mr-2 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-M-Lime dark:text-D-Gray-dark dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"
+        className="py-2.5 px-5 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-M-Lime dark:text-D-Gray-dark dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"
         onClick={() => {
           localStorage.removeItem("token");
           history.push("/");
