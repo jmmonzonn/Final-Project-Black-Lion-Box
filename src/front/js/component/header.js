bad70e6260c3ca55f31e91da3eb2ba76076653e9
jorgepardor <jorgeluis.mail@gmail.com>
2022-05-16 07:29:52 +0000

    Probando soluciones al zoom en los componentes -WIP

--- src/front/js/component/header.js
+++ src/front/js/component/header.js
@@ -4,10 +4,10 @@ import { Alert } from "../component/alert";
 export const MainHeader = () => {
   return (
     <div>
-      <div className="bg-headerbg text-center dark:bg-headerbgD mb-12 w-full h-screen">
+      <div className="bg-headerbg text-center dark:bg-headerbgD mb-12 w-full h-max">
         <div className="col-auto">
           <video
-            className="logoheader sm:w-2/5 w-3/4 mx-auto pt-12 max-h-120"
+            className="logoheader sm:scale-75 h-3/4 mx-auto pt-4"
             src="https://res.cloudinary.com/blacklionbox/video/upload/v1650464596/hola_VP9_1_rmst1a.webm"
             autoPlay
             loop
@@ -15,14 +15,6 @@ export const MainHeader = () => {
           />
         </div>
         <div className="col-auto">
-          {/* <div className=" pt-14">
-            <h2
-              className="hero glitch layers font-bellfort"
-              data-text="NO PAIN, NO GAIN"
-            >
-              <span>NO PAIN, NO GAIN</span>
-            </h2>
-          </div> */}
           <div className="pt-4">
             <p className="sm:w-1/4 w-4/5 display-5 text-L-Gray-light text-2xl font-medium opacity-75 mx-auto">
               REGÍSTRATE ANTES DEL 31/05 Y DISFRUTA DEL PRIMER MES CON UN
@@ -32,11 +24,10 @@ export const MainHeader = () => {
           </div>
           <div>
             <a href="#Suscripciones">
-              <button className="mt-5 text-L-Gray-dark focus:ring-4 focus:outline-none rounded-lg text-4xl px-6 py-3.5 text-center font-bellfort bg-M-Lime">
+              <button className="mt-5 mb-20 text-L-Gray-dark focus:ring-4 focus:outline-none rounded-lg text-4xl px-6 py-3.5 text-center font-bellfort bg-M-Lime">
                 APÚNTATE AHORA!
               </button>
             </a>
-            {/* <Alert content="Test" style="bg-M-Lime text-L-Gray-light" /> */}
           </div>
         </div>
         <div />
